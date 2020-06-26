d404ed86be151be5893018056ab120dd80918b36
Batstone <adam.batstone@gmail.com>
2020-06-25 19:41:12 -0500

    Initial coommit

---
+++ resources/js/script.js
@@ -0,0 +1,30 @@
+
+
+
+$(document).ready(function() {
+    
+    $('.js--main-intro').waypoint(function(direction) {
+        if (direction === "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    
+    }, {
+        offset: '40px;'
+    });
+
+
+
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+
+        nav.slideToggle(200)
+
+    });
+
+
+
+});
+
