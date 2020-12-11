80f653cf8eae38a632862b8987b22e5807d23fd9
Batstone <adam.batstone@gmail.com>
2020-12-11 15:55:47 -0600

    removed the contant form

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,23 +1,20 @@
 
 
 
-$(document).ready(function() {
-    
-    $('.js--main-intro').waypoint(function(direction) {
+$(document).ready(function () {
+
+    $('.js--main-intro').waypoint(function (direction) {
         if (direction === "down") {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky');
         }
-    
+
     }, {
         offset: '40px;'
     });
 
-
-
-
-    $('.js--nav-icon').click(function() {
+    $('.js--nav-icon').click(function () {
         var nav = $('.js--main-nav');
 
         nav.slideToggle(200)
